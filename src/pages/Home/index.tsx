d20c5a83cd7a1fb89fd36daf4605dfd64c0cf29f
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-18 16:35:12 -0300

    add: axios to connect to github api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
+import { useCallback, useEffect } from "react";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { HomeContainer, PostsContainer } from "./styles";
+import { api } from "../../lib/axios";
 
-function Home(){
+const username = import.meta.env.USERNAME;
+const repoName = import.meta.env.REPO_NAME;
+
+function Home() {
 
   const posts = [
     {
@@ -17,19 +22,15 @@ function Home(){
       title: "JavaScript data types and data structures",
       content: "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.",
       publishedAt: "há 1 dia"
-    },
-    {
-      id: "125",
-      title: "JavaScript data types and data structures",
-      content: "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.",
-      publishedAt: "há 1 dia"
-    },{
-      id: "126",
-      title: "JavaScript data types and data structures",
-      content: "Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.",
-      publishedAt: "há 1 dia"
     }
   ];
+  const getPosts = useCallback(async (query = "") => {
+    const data = await api.get("/");
+
+  }, []);
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <HomeContainer className="container">
@@ -37,7 +38,7 @@ function Home(){
       <SearchInput />
       <PostsContainer>
         {posts.map((post) => (
-          <Post 
+          <Post
             key={post.id}
             title={post.title}
             content={post.content}
